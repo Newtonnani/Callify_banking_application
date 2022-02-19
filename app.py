34509eae8dd0062838f348b133e430157846cc7b
Newton Sathyavety <newton.sathyavety@intimetec.com>
2022-02-19 19:59:53 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+from datetime import date, datetime
+from sqlalchemy import or_, and_
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Account
+from models import Users
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/user", methods=['POST', 'GET'])
+def add_user():
+    if request.method == 'POST':
+        user_name = request.json.get('user_name')
+        account_number = request.json.get('account_number')
+        try:
+            user = Users(
+                user_name=user_name,
+                account_number=account_number
+            )
+            db.session.add(user)
+            db.session.commit()
+            return "User added. user id={}".format(user.id)
+        except Exception as e:
+            return str(e)
+    else:
+        # user_mapped = Users.query.filter(Users.account_number == 123123123123).first()
+        # print(user_mapped.id)
+        # print(user_mapped.user_name)
+        # print(user_mapped.account_number)
+        all_users = Users.query.all()
+        return jsonify([e.serialize() for e in all_users])
+
+
+@app.route("/account", methods=['POST'])
+def add_account():
+    try:
+        account_number = request.json.get('Account No')
+        date = request.json.get('Date')
+        transaction_details = request.json.get('Transaction Details')
+        value_date = request.json.get('Value Date')
+        withdraw_amount = request.json.get('Withdrawal AMT')
+        deposit_amount = request.json.get('Deposit AMT')
+        balance_amount = request.json.get('Balance AMT')
+        user_mapped = Users.query.filter(Users.account_number == account_number).first()
+        print(user_mapped)
+        account_trans_user = Account(
+            account_number_id=user_mapped.id,
+            date=date,
+            transaction_details=transaction_details,
+            value_date=value_date,
+            withdraw_amount=withdraw_amount,
+            deposit_amount=deposit_amount,
+            balance_amount=balance_amount
+        )
+        db.session.add(account_trans_user)
+        db.session.commit()
+        return "account transaction added for this user {}".format(user_mapped.user_name)
+    except Exception as e:
+        return str(e)
+
+
+@app.route("/<user>/transactions/<date>", methods=['GET'])
+def transactions_user_date(user=None, date=None):
+    try:
+        date_process = str(datetime.strptime(date, '%d-%m-%y').strftime("%d %b %y"))
+        # print(date,date_process)
+        user_mapped = Users.query.filter(Users.user_name == user).first()
+        account_mapped = Account.query.filter(and_(
+            Account.account_number_id == user_mapped.id, Account.date == date_process)).all()
+        return jsonify([e.serialize() for e in account_mapped])
+    except Exception as e:
+        return str(e)
+
+
+@app.route("/<user>/details/<ID>", methods=['GET'])
+def transactions_user_id(user=None, ID=None):
+    try:
+        user_mapped = Users.query.filter(Users.user_name == user).first()
+        account_mapped = Account.query.filter(and_(
+            Account.account_number_id == user_mapped.id, Account.id == int(ID))).first()
+        return jsonify([account_mapped.serialize()])
+    except Exception as e:
+        return str(e)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,64 @@
+from app import db
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(), nullable=False)
+    account_number = db.Column(db.BigInteger, unique=True, nullable=False)
+    account = db.relationship('Account', backref='users', lazy=True)
+
+    # account_number = db.Column(db.BigInteger(), db.ForeignKey('accounts.account_number'),
+    #                            nullable=False)
+    def __init__(self, user_name, account_number):
+        self.user_name = user_name
+        self.account_number = account_number
+
+    def __repr__(self):
+        return f"id:{self.id} user_name:{self.user_name} account_number:{self.account_number}"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_name': self.user_name,
+            'account_number': self.account_number
+        }
+
+
+class Account(db.Model):
+    __tablename__ = 'account'
+    id = db.Column(db.Integer, primary_key=True)
+    # account_number = db.relationship('Users', backref='Accounts', lazy=True)
+    account_number_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                                  nullable=False)
+    date = db.Column(db.String())
+    transaction_details = db.Column(db.String())
+    value_date = db.Column(db.String())
+    withdraw_amount = db.Column(db.String())
+    deposit_amount = db.Column(db.String())
+    balance_amount = db.Column(db.String())
+
+    def __init__(self, account_number_id, date, transaction_details, value_date, withdraw_amount, deposit_amount,
+                 balance_amount):
+        self.account_number_id = account_number_id
+        self.date = date
+        self.transaction_details = transaction_details
+        self.value_date = value_date
+        self.withdraw_amount = withdraw_amount
+        self.deposit_amount = deposit_amount
+        self.balance_amount = balance_amount
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'account_number_id': self.account_number_id,
+            'date': self.date,
+            'transaction_details': self.transaction_details,
+            'value_date': self.value_date,
+            'withdraw_amount': self.withdraw_amount,
+            'deposit_amount': self.deposit_amount,
+            'balance_amount': self.balance_amount
+        }
